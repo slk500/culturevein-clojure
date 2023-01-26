20cb5a9e80363a08bc33ab341cad1d0cae2d0b2e
slk500 <slawomir.grochowski@gmail.com>
2023-01-26 21:26:29 +0100

    add recursive parent-child list tags

--- src/frontend/culturevein.cljs
+++ src/frontend/culturevein.cljs
@@ -1,9 +1,11 @@
 (ns ^:figwheel-hooks frontend.culturevein
+  (:require-macros [hiccups.core :as hiccups])
   (:require
   [goog.dom :as gdom]
   [reagent.core :as r :refer [atom]]
   [reagent.dom :as rdom]
   [ajax.core :as ajax]
+  [hiccups.runtime]
   (clojure.string :as str)))
 
 (def value (r/atom ""))
@@ -11,7 +13,7 @@
                           :tags {}}))
 
 (defn handle-response [resp]
-  (swap! app-state assoc :tags (get-in resp ["data"])))
+  (swap! app-state assoc :tags (clojure.walk/keywordize-keys (get-in resp ["data"]))))
 
 (defn get-tags []
   (ajax/GET "http://localhost:8000/api/tags"
@@ -21,10 +23,10 @@
 
 (defn title []
   [:h1 (:title @app-state)])
-
+ 
 (defn shared-state []
   [:div
-   [:p "The value is now: " @value]])
+   [:p "the value is now: " @value]])
 
 (defn main-search []
   [:input {:type "text"
@@ -33,25 +35,47 @@
 
 (defn highlight [string search] 
   (clojure.string/replace string 
-                          (js/RegExp. (str "("search")") "iu") "<span class='highlight'>$1</span>")
+                          (js/RegExp. (str "("search")") "iu") "<span class='highlight'>$1</span>"))
+
+;; (defn list-tag [tags]
+;;   [:ul (doall (for [tag tags]
+;;                 [:li 
+;;                  {:dangerouslySetInnerHTML {:__html (highlight (:tag_name tag) @value)}}]                
+;;                 ))]
+;; )
+
+(defn map-to-html-list [mapa]
+  (hiccups/html
+   [:ul
+    (for [tag mapa]
+      [:li 
+       (str (:tag_name tag) 
+            (if (seq (:children tag))
+              (map-to-html-list (:children tag))))
+       ])
+    ]))
+
+(defn list-tag [tags]
+  [:ul (for [tag tags]
+                [:li (:tag_name tag)])]
   )
 
 (defn list-tags [items]
   (let [results (for [item items
-                      :when (str/includes? (get-in item ["tag_name_lowercase"]) @value)] item)]
+                      :when (str/includes? (:tag_name_lowercase item) @value)] item)]
     [:div
-     [:p "counter: " (count results)]
-     [:ul (doall (for [result results]
-                   [:li 
-{:dangerouslySetInnerHTML {:__html (highlight (get-in result ["tag_name"]) @value)}}
-]))]]))
+     [:p (count results) " tags"]
+     (list-tag results)
+]))
 
 (defn app []
   [:div.app
    [title]
+   [:div {:dangerouslySetInnerHTML {:__html (map-to-html-list (:tags @app-state))}}]
    [main-search]
    [shared-state]
-   [list-tags(:tags @app-state)]]
+   [list-tags (:tags @app-state)]
+]
   )
 
 (defn mount-app-element []                                 
--- test/frontend/culturevein_test.cljs
+++ test/frontend/culturevein_test.cljs
@@ -1,9 +1,15 @@
 (ns frontend.culturevein-test
     (:require
-     [cljs.test :refer-macros [deftest is testing]]))
+     [cljs.test :refer-macros [deftest is testing run-tests]]))
 
 (deftest addition-tests
   (is (= 5 (+ 3 2)))
-  (is (= 2 (+ 3 2)))
   (is (= 10 (+ 5 5))))
+
+(deftest flatten-children-relation
+  (is (= [
+          {"name" "parent", "children" []} 
+          {"name" "child", "children" []}]
+         [
+          {"name" "parent", "children" [{"name" "parent", "children" []}]}])))
 
