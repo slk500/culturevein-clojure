1904a1c3a2b32221aacab739d95c4d0b95e9fa14
slk500 <slawomir.grochowski@gmail.com>
2023-02-07 12:52:12 +0100

    add simple routing

--- src/frontend/culturevein.cljs
+++ src/frontend/culturevein.cljs
@@ -1,16 +1,21 @@
 (ns ^:figwheel-hooks frontend.culturevein
   (:require-macros [hiccups.core :as hiccups])
   (:require
-  [goog.dom :as gdom]
-  [reagent.core :as r :refer [atom]]
-  [reagent.dom :as rdom]
-  [ajax.core :as ajax]
-  [hiccups.runtime]
-  [clojure.string :as str]))
+   [goog.dom :as gdom]
+   [reagent.core :as r :refer [atom]]
+   [reagent.dom :as rdom]
+   [ajax.core :as ajax]
+   [hiccups.runtime]
+   [bide.core :as b]
+   [clojure.string :as str]))
+
+(declare page-music-videos)
+(declare mount-app-element)
 
 (def value (r/atom ""))
 (defonce app-state (atom {:title "CultureVein"
-                          :tags {}}))
+                          :tags {}
+                          :current-route [:home]}))
 
 (defn handle-response [resp]
   (swap! app-state assoc :tags (clojure.walk/keywordize-keys (get-in resp ["data"]))))
@@ -21,51 +26,65 @@
 
 (get-tags)
 
-(defn title []
-  [:h1 (:title @app-state)])
- 
+(defn mount-page-music-videos []                                 
+  (rdom/render [page-music-videos] (gdom/getElement "app")))
+
 (defn main-search []
   [:div
    [:input {:type "text"
             :value @value
-            :on-change #(reset! value (-> % .-target .-value))}]
-   [:p "the value is now: " @value]]
-)
+            :on-change #(reset! value (-> % .-target .-value))}]]
+  )
+
+(defn highlight [s search]
+  (if (str/blank? search)
+    s
+   (str/replace s 
+                 (js/RegExp. (str "("search")") "iu") "<span class='highlight'>$1</span>")))
 
 (defn includes-in-tags-tree? [tags substr]
   (->> (tree-seq associative? identity tags)
-       (some #(and (map-entry? %)
+       (some #(if (map-entry? %)
                    (let [[k v] %]
-                     (and (= k :tag_name_lowercase)
+                     (if (= k :tag_name_lowercase)
                           (str/includes? v substr)))))))
 
 (defn tags-to-html-list [tags]
   (hiccups/html
-   [:ul
-    (for [tag tags]  
-      [:li 
-       (str (:tag_name tag) 
-            (if (seq (:children tag))
-              (tags-to-html-list (:children tag))))
-       ])
-    ]))
+   [:ul {:class ".container"}
+    (for [tag tags]
+      [:li
+       (let [tag-name (:tag_name tag)
+             children-tag (:children tag)]
+         (str (highlight tag-name @value)
+              (if (seq children-tag)
+                (tags-to-html-list children-tag))))])]))
 
 (defn list-tags [items]
   (let [results (for [item items
                       :when (includes-in-tags-tree? item @value)] item)]
-    [:div {:dangerouslySetInnerHTML {:__html (tags-to-html-list results)}}]
-  ))
+    [:div {:dangerouslySetInnerHTML {:__html (tags-to-html-list results)}}]))
 
-(defn app []
+(defn navbar []
+  [:div 
+   [:h1 (:title @app-state)]
+   [:ul
+    [:li [:a {:href "#" :on-click mount-app-element} "tags"]]
+    [:li [:a {:href "#" :on-click mount-page-music-videos} "music videos"]]
+    [:li {:href "#"} "add music video"]]
+   [main-search]])
+
+(defn page-music-videos []
   [:div.app
-   [title]
-   [main-search]
-   [list-tags (:tags @app-state)]
- ]
-  )
+   [navbar]])
+
+(defn page-tags []
+  [:div.app
+   [navbar]
+   [list-tags (:tags @app-state)]])
 
 (defn mount-app-element []                                 
-  (rdom/render [app] (gdom/getElement "app")))
+  (rdom/render [page-tags] (gdom/getElement "app")))
 
 (mount-app-element)
 
