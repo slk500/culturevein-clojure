cddcd611a0893dc3fa986cc54405b2ee01b577d8
slk500 <slawomir.grochowski@gmail.com>
2023-02-20 21:40:41 +0100

    add list video tags time under music video

--- src/frontend/api.cljs
+++ src/frontend/api.cljs
@@ -4,18 +4,24 @@
 
 (defn get-tag-list [app-state]
   (ajax/GET "http://localhost:8000/api/tags"
-    {:handler #(swap! app-state assoc :tags (:data %))
-     :response-format :json
-     :keywords? true}))
+            {:handler #(swap! app-state assoc :tags (:data %))
+             :response-format :json
+             :keywords? true}))
 
 (defn get-music-video-list [app-state]
   (ajax/GET "http://localhost:8000/api/videos"
-    {:handler #(swap! app-state assoc :music-videos (:data %))
-     :response-format :json
-     :keywords? true}))
+            {:handler #(swap! app-state assoc :music-videos (:data %))
+             :response-format :json
+             :keywords? true}))
 
 (defn get-music-video-tag-list [music-video-state youtube-id]
   (ajax/GET (str "http://localhost:8000/api/videos/" youtube-id "/tags")
-    {:handler #(swap! music-video-state assoc :tags (:data %))
-     :response-format :json
-     :keywords? true}))
+            {:handler #(swap! music-video-state assoc :tags (:data %))
+             :response-format :json
+             :keywords? true}))
+
+(defn get-music-video [music-video-state youtube-id]
+  (ajax/GET (str "http://localhost:8000/api/videos/" youtube-id)
+            {:handler #(swap! music-video-state assoc :video (first (:data %)))
+             :response-format :json
+             :keywords? true}))
--- src/frontend/music_video.cljs
+++ src/frontend/music_video.cljs
@@ -2,28 +2,42 @@
   (:require
    [reagent.core :as r :refer [atom]]
    [frontend.api :as api]
+   [goog.string :refer [format]]
    ["react-youtube$default" :as YouTube])
   (:refer-clojure :exclude [list]))
 
 (defonce music-video-state (atom {:youtube-id ""
-                                  :tags []}))
+                                  :tags []
+                                  :video {}}))
+
+(defn seconds-to-time-string [seconds]
+  (let [minutes (quot seconds 60)
+        seconds (mod seconds 60)]
+    (str (format "%2d" minutes) ":" (format "%02d" seconds))))
 
 (defn show [youtube-id]
   (r/create-class
    {:component-did-mount
     (fn []
       (swap! music-video-state assoc :youtube-id youtube-id)
-      (api/get-music-video-tag-list music-video-state youtube-id))
+      (api/get-music-video-tag-list music-video-state youtube-id)
+      (api/get-music-video music-video-state youtube-id))
     :reagent-render
     (fn []
       [:div
-       ;; [:> YouTube
-       ;;  {:videoId youtube-id}]
-       [:div "ssssssssss"
-        [:ul
-         (for [tag (:tags @music-video-state)]
-           ^{:key (:video-_tag_id tag)}
-           [:li (:tag_name tag)])]]])}))
+       [:> YouTube
+        {:videoId youtube-id}]
+       [:a {:href (str "/artists/" (-> @music-video-state :video :artist_slug))} (-> @music-video-state :video :artist_name)]
+       [:span " - " (-> @music-video-state :video :video_name)]
+       [:ul {:class "list-unstyled"}
+        (for [tag (:tags @music-video-state)]
+          ^{:key (:video_tag_id tag)}
+          [:li (:tag_name tag)
+           (when-let [tags-time (:video_tags_time tag)]
+             [:ol
+              (for [tag-time tags-time]
+                ^{:key (:video_tag_time_id tag-time)}
+                [:li (str (seconds-to-time-string (:start tag-time)) " -" (seconds-to-time-string (:end tag-time)))])])])]])}))
 
 (defn list [artists]
   [:div
