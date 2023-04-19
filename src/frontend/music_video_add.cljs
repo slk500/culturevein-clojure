b4f602c1b2aa2dc4797acda58c5543b5f7b67296
Sławomir Grochowski <slawomir.grochowski@gmail.com>
2023-04-19 20:02:58 +0200

    music video add: embeding & data read works

--- src/frontend/music_video_add.cljs
+++ src/frontend/music_video_add.cljs
@@ -6,9 +6,10 @@
    [reagent.core :as r :refer [atom]]))
 
 (defonce pasted-link (atom ""))
-(defonce player (atom nil))
+(defonce player nil)
 (defonce music-video-data (atom {:artist ""
-                                 :title  ""}))
+                                 :title  ""
+                                 :duration 0}))
 
 (defn embed-player [youtube-id]
   (let [player-div (gdom/createElement "div")]
@@ -20,7 +21,8 @@
                                               :events #js {:onReady (fn []
                                                                       (let [[artist title] (map str/trim (str/split (.. (.getVideoData player) -title) #"-"))]
                                                                         (swap! music-video-data assoc :artist artist)
-                                                                        (swap! music-video-data assoc :title title)))
+                                                                        (swap! music-video-data assoc :title title))
+                                                                      (swap! music-video-data assoc :duration (. player getDuration)))
                                                            :onStateChange (fn [] (.log js/console "state change"))}}))))
 
 (defn youtube-id [pasted-link]
@@ -28,7 +30,15 @@
             (re-matches #"^(?:https?:)?//[^/]*(?:youtube(?:-nocookie)?\.com|youtu\.be).*[=/]([-\w]{11})(?:\?|=|&|$)"
                         pasted-link)]
     (embed-player youtube-id)
-    (gdom/removeNode (gdom/getElement "player"))))
+    (do
+      (gdom/removeNode (gdom/getElement "player"))
+      (swap! music-video-data assoc-in [:title] "")
+      (swap! music-video-data assoc-in [:artist] ""))))
+
+
+(defn update-data [data key]                               ;; <1>
+  (fn [e]
+    (swap! data assoc key (.. e -target -value))))
 
 (defn page []
   [:div
@@ -38,7 +48,16 @@
                          (reset! pasted-link (-> evt .-target .-value))
                          (youtube-id (-> evt .-target .-value)))
             :placeholder "paste youtube link here"}]
-   (form/form-field "id" "artist")
-   (form/form-field "id" "title")
+   [:input {:id "artist"
+            :type "text"
+            :value (get @music-video-data :artist "")
+            :on-change (update-data music-video-data :artist)
+            :placeholder "artist"}]
+   [:input {:id "title"
+            :type "text"
+            :value (get @music-video-data :title "")
+            :on-change (update-data music-video-data :title)
+            :placeholder "title"}]
+   [:p "https://www.youtube.com/watch?v=WvfEP_1sdtE"]
    (form/action-button "button" "add music video")
    [:div#wrapper]])
